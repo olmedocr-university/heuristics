bcae28a9ff193ac59fa8d71cc503fe8fb9058dea
Raul Olmedo <20819857+olmedocr@users.noreply.github.com>
2019-11-18 11:55:54 +0100

    Fixed concept errors and added teachers section

--- p2-383355-346073/parte-1/schedule.py
+++ p2-383355-346073/parte-1/schedule.py
@@ -5,20 +5,31 @@ tue = range(4, 7)
 wed = range(8, 11)
 thu = range(12, 14)
 
-NSC = ['NSC1', 'NSC2']
-HSC = ['HSC1', 'HSC2']
-SP = ['SP1', 'SP2']
-MAT = ['MAT1', 'MAT2']
-EN = ['EN1', 'EN2']
-PE = ['PE']
+first = [0, 4, 8, 12]
 
-var_dict = {
-    'NSC': [2, 6, 10, 13],
-    'HSC': [0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13],
-    'SP': [0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13],
-    'MAT': [0, 4, 8, 12],
-    'EN': [0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13],
+# Mon@1st = 0, ... , Tue@1st = 4, ... , Thu@2nd = 13
+subjects = {
+    'NSC1': [2, 6, 10, 13],
+    'NSC2': [2, 6, 10, 13],
+    'HSC1': [0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13],
+    'HSC2': [0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13],
+    'SP1': [0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13],
+    'SP2': [0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13],
+    'MAT1': [0, 4, 8, 12],
+    'MAT2': [0, 4, 8, 12],
+    'EN1': [0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13],
+    'EN2': [0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13],
     'PE': [0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13]
+}
+
+# NSC = 0, HSC = 1, ... , PE = 5
+teachers = {
+    'LUC1': [1],
+    'LUC2': [0, 1, 2, 3, 4, 5],
+    'AND1': [5],
+    'AND2': [0, 1, 2, 3, 4, 5],
+    'JUA1': [0, 1, 2, 3, 4, 5],
+    'JUA2': [0, 1, 2, 3, 4, 5],
 }
 
 
@@ -39,37 +50,38 @@ def is_not_on_the_same_day(a, b):
         return False
 
 
+def is_not_at_first_hour(a):
+    return a not in first
+
+
 problem = Problem()
 
-problem.addVariable('NSC1', [2, 6, 10, 13])
-problem.addVariable('NSC2', [2, 6, 10, 13])
-problem.addVariable('HSC1', [0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13])
-problem.addVariable('HSC2', [0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13])
-problem.addVariable('SP1', [0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13])
-problem.addVariable('SP2', [0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13])
-problem.addVariable('MAT1', [0, 4, 8, 12])
-problem.addVariable('MAT2', [0, 4, 8, 12])
-problem.addVariable('EN1', [0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13])
-problem.addVariable('EN2', [0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13])
-problem.addVariable('PE', [0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13])
+for key, value in subjects.items():
+    problem.addVariable(key, value)
+
+for key, value in teachers.items():
+    problem.addVariable(key, value)
 
 # All subjects must be in different time slots
-problem.addConstraint(AllDifferentConstraint(),
-                      ['NSC1', 'NSC2', 'HSC1', 'HSC2', 'SP1', 'SP2', 'MAT1', 'MAT2', 'EN1', 'EN2', 'PE'])
+problem.addConstraint(AllDifferentConstraint(), [*subjects.keys()])
 
-# Natural science class must be consecutive
-problem.addConstraint(is_consecutive, ('NSC1', 'NSC2'))
+# Human & social science class must be consecutive
+problem.addConstraint(is_consecutive, ('HSC1', 'HSC2'))
 
-# MAT-NSC & MAT-SP cannot be taught in the same day
+# MAT-NSC & MAT-EN cannot be taught on the same day
+# TODO: rewrite this to include lists instead of manually performing the cartesian product
 problem.addConstraint(is_not_on_the_same_day, ('MAT1', 'NSC1'))
 problem.addConstraint(is_not_on_the_same_day, ('MAT1', 'NSC2'))
 problem.addConstraint(is_not_on_the_same_day, ('MAT2', 'NSC1'))
 problem.addConstraint(is_not_on_the_same_day, ('MAT2', 'NSC2'))
-problem.addConstraint(is_not_on_the_same_day, ('MAT1', 'SP1'))
-problem.addConstraint(is_not_on_the_same_day, ('MAT1', 'SP2'))
-problem.addConstraint(is_not_on_the_same_day, ('MAT2', 'SP1'))
-problem.addConstraint(is_not_on_the_same_day, ('MAT2', 'SP2'))
+problem.addConstraint(is_not_on_the_same_day, ('MAT1', 'EN1'))
+problem.addConstraint(is_not_on_the_same_day, ('MAT1', 'EN2'))
+problem.addConstraint(is_not_on_the_same_day, ('MAT2', 'EN1'))
+problem.addConstraint(is_not_on_the_same_day, ('MAT2', 'EN2'))
 
-print(problem.getSolutions())
+# All teachers must lecture different subjects
+problem.addConstraint(AllDifferentConstraint(), [*teachers.keys()])
 
+# problem.addConstraint(is_not_at_first_hour, )
 
+print(problem.getSolution())
